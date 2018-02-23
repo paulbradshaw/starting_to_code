9bd96ce748effcf439fdb7c28b87649315e05e67
Paul Bradshaw <paulonhismobile@gmail.com>
2018-02-23 07:09:26 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -10,7 +10,8 @@ html = scraperwiki.scrape("http://foo.com")
 #
 # # Find something on the page using css selectors
 root = lxml.html.fromstring(html)
-root.cssselect("div[align='left']")
+print root
+print root.cssselect("div[align='left']")
 #
 # # Write out to the sqlite database using scraperwiki library
 # scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
